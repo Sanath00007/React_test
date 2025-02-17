8a43bcd9c26712477b8c90c30f2702c41cffeafe
sanath00007 <sanathk00007@gmail.com>
2025-02-17 23:33:15 +0530

    some change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,7 +6,7 @@ function TextForm(props) {
   <h1>{props.heading}</h1>   
 <div className="mb-3">
 
-<textarea className="form-control" id="MyBox" rows="8" columns="5"></textarea>
+<textarea className="form-control" id="MyBox" rows="8" ></textarea>
 </div>
       
     </div>
